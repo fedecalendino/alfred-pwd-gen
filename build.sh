d3564f2b4a8b9e8a6a1873cf744c4ed8838bc74f
Fede Calendino <fede@calendino.com>
2022-09-01 10:50:18 +0200

    Migrate to nuitka

--- build.sh
+++ build.sh
@@ -1,4 +1,20 @@
-poetry run pyinstaller --onefile --paths ./src/ src/main.py
+function build() {
+  echo "Generating binary file for $1"
+  echo
 
-rm -rf ./build
-rm *.spec
+  poetry run python3 -m nuitka --onefile "src/$1.py"
+
+  echo
+  echo "Cleaning up nuitka temporary files..."
+   rm -rf "$1.build/"
+   rm -rf "$1.dist/"
+   rm -rf "$1.onefile-build/"
+
+	 mkdir bin 2> /dev/null
+	 mv "$1.bin" "./bin/$1"
+
+  echo
+	echo "Finished generating bin file for $1"
+}
+
+build main
